ad4bb02af8dbfdfee1262b2c911ad8d79d0cf87c
Charles Copley <ccopley@Charless-MacBook-Pro.local>
2022-02-24 13:26:05 -0500

    minor mods

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-import openpyxl
+import pandas as pd
+
 
 
 def index(request):
@@ -7,37 +8,16 @@ def index(request):
         return render(request, 'myapp/index.html', {})
     else:
         excel_file = request.FILES["excel_file"]
+        dfs = pd.read_excel(excel_file, sheet_name='Sheet1')
+        field_names = ["First Collection", "Second Collection", "Third Collection"]
+        output = pd.DataFrame()
+        for field in field_names:
+            group_val = pd.DataFrame(dfs.groupby([field, "Farm", "House"]).size())
+            output = pd.concat([output, group_val])
+        output.columns = ['Count']
+        ret_html = output.to_html()
 
-        # you may put validations here to check extension or file size
-
-        wb = openpyxl.load_workbook(excel_file)
-
-        # getting all sheets
-        sheets = wb.sheetnames
-        print(sheets)
-
-        # getting a particular sheet
-        worksheet = wb["Sheet1"]
-        print(worksheet)
-
-        # getting active sheet
-        active_sheet = wb.active
-        print(active_sheet)
-
-        # reading a cell
-        print(worksheet["A1"].value)
-
-        excel_data = list()
-        # iterating over the rows and
-        # getting value from each cell in row
-        for row in worksheet.iter_rows():
-            row_data = list()
-            for cell in row:
-                row_data.append(str(cell.value))
-                print(cell.value)
-            excel_data.append(row_data)
-
-        return render(request, 'myapp/index.html', {"excel_data":excel_data})
+        return render(request, 'myapp/index.html', {"excel_data":ret_html})
 
 
 
